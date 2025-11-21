7ddbea0b83e040b0cd689dce76a7db88ec078d92
nico3807 <nicolas.maurin1@umontpellier.fr>
2025-11-21 23:15:39 +0100

    d

--- script.js
+++ script.js
@@ -11,8 +11,7 @@ const codeMirrorInstance = CodeMirror.fromTextArea(editorElement, {
 // Clé API et Configuration
 const API_KEY = "AIzaSyCwlGNH4z-pqb0b3GbP4dyACEu5dDiMJ_o";
 
-const API_URL =
-  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key=${CONFIG.API_KEY}";
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key=${API_KEY}`;
 
 // Éléments du DOM
 const exerciseContainer = document.getElementById("exerciseContainer");
